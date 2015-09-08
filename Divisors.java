e8f1cda3fcfa8f2dc1510d5e3d4828d7c20540d4
Kent Collins <kent.r.collins@gmail.com>
2015-09-08 06:47:47 +0100

    Create Divisors.java

---
+++ Divisors.java
@@ -0,0 +1,11 @@
+/**
+* Methods for working with numbers that evenly divisible into
+* other numbers.  Not particularly useful, except as an illustration
+* of how to define, populate, and return an array
+*
+* @version September 8, 2015
+*/
+
+public class Divisors {
+
+}
