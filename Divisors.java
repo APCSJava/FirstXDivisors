d94c1b6f142cd452e6dd2057521974e0beb40112
Kent Collins <kent.r.collins@gmail.com>
2015-09-08 06:00:07 +0000

    implemented divisors

--- Divisors.java
+++ Divisors.java
@@ -1,5 +1,5 @@
 /**
-* Methods for working with numbers that evenly divisible into
+* Methods for working with numbers that are evenly divisible into
 * other numbers.  Not particularly useful, except as an illustration
 * of how to define, populate, and return an array
 *
@@ -7,5 +7,31 @@
 */
 
 public class Divisors {
+    
+    // test whether an int is evenly divisible by another
+    public static boolean isDivisible(int value, int divisor) {
+        return (value%divisor==0);
+    }
+    
+    public static int[] getFirstXDivisors (int x, int start, int divisor){
+        int[] results = new int[x];
+        int numFound = 0;
+        int currentValue = start;
+        while (numFound<x) {
+            if (isDivisible(currentValue, divisor)) {
+                results[numFound] = currentValue;
+                numFound++;
+            }
+            currentValue++;
+        }
+      return results;
+    }
 
+    // any class can declare a main method -- doesn't mean 'should', but 'can'
+    public static void main(String[] args) {
+        int[] first5SevensOver100 = getFirstXDivisors(5, 100, 7);
+        for (int i: first5SevensOver100) {
+            System.out.println(i);
+        }
+    }
 }
